61db5ccbcd946e0e4f9dfea802335193f556c4bf
andradeju <julliana.andrade08@gmail.com>
2023-03-21 13:48:21 -0300

    feat: rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Menu } from './components/Menu/Menu';
+import { Blog } from './pages/Blog/Blog';
+import { Contato } from './pages/Contato/Contato';
+import { Home } from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>ola terraqueos</h1>
+      <BrowserRouter>
+      <Menu/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/blog' element={<Blog />}/>
+          <Route path='/contato' element={<Contato />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
